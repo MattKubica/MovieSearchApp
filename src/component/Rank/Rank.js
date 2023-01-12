7e3cf33fd7ca3c76612cf298d879f6c69df58974
Matúš <19matus98@gmail.com>
2023-01-12 19:12:05 +0100

    rak update

--- src/component/Rank/Rank.js
+++ src/component/Rank/Rank.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./Rank.css";
-import FaceRecognition from "../FaceRecognition/FaceRecognition";
+
 function Rank() {
   return (
     <div className="rankFrame">
       <div className="rankText__name">
-        <p>MovieSearchApp</p>
-      </div>
-      <div className="rankText__text__holder">
-        <p> Movie</p>
+        <p>Movie Search App</p>
       </div>
     </div>
   );
