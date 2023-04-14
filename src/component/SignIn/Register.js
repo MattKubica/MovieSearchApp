04f1767c0bbb0bea8bb819ddf28d7322da7a010c
Matúš <19matus98@gmail.com>
2023-04-15 00:16:25 +0200

    snaegel

--- src/component/SignIn/Register.js
+++ src/component/SignIn/Register.js
@@ -17,7 +17,7 @@ function Register({ onRouteChange }) {
         throw new Error("Please fill in all fields");
       }
 
-      fetch("https://moviedb-rlml.onrender.com", {
+      fetch("http://localhost:3000/register", {
         method: "post",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
